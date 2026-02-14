88eaf53dc8e65e6e0f81d39ff5dbbe96ab0119e0
Cauã Lima <cauai8099@gmail.com>
2026-02-14 20:38:11 -0300

    Loop adicionado e possibilidade de calcular porcentagem, potenciação e resto de divisão

--- calculadora.py
+++ calculadora.py
@@ -1,28 +1,47 @@
-print("Calculadora Simples")
-print("1 - Soma")
-print("2 - Subtração")
-print("3 - Multiplicação")
-print("4 - Divisão")
+while True:
+    print("\nCalculadora")
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    print("5 - Resto da divisão")
+    print("6 - Potenciação")
+    print("7 - Porcentagem")
+    print("0 - Sair")
 
-opcao = input("Escolha a operação: ")
+    opcao = input("Escolha a operação: ")
 
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
+    if opcao == "0":
+        print("Encerrando...")
+        break
 
-if opcao == "1":
-    resultado = num1 + num2
-elif opcao == "2":
-    resultado = num1 - num2
-elif opcao == "3":
-    resultado = num1 * num2
-elif opcao == "4":
-    if num2 != 0:
-        resultado = num1 / num2
+    if opcao in ["1", "2", "3", "4", "5", "6", "7"]:
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+
+        if opcao == "1":
+            resultado = num1 + num2
+        elif opcao == "2":
+            resultado = num1 - num2
+        elif opcao == "3":
+            resultado = num1 * num2
+        elif opcao == "4":
+            if num2 != 0:
+                resultado = num1 / num2
+            else:
+                print("Erro: divisão por zero")
+                continue
+        elif opcao == "5":
+            if num2 != 0:
+                resultado = num1 % num2
+            else:
+                print("Erro: divisão por zero")
+                continue
+        elif opcao == "6":
+            resultado = num1 ** num2
+        elif opcao == "7":
+            resultado = (num1 * num2) / 100
+
+        print("Resultado:", resultado)
     else:
-        print("Erro: divisão por zero")
-        exit()
-else:
-    print("Opção inválida")
-    exit()
-
-print("Resultado:", resultado)
+        print("Opção inválida")
